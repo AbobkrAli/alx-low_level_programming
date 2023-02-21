ad0fb3dfc08cc2b52b95f02d70ec9ef06608d792
abobkr ali <abobkracc@gmail.com>
2023-02-21 14:19:04 +0200

    adding 2 th file 1 th try

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -  prints form a to z
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	for(char i ='a'; i >= 'z';i++)
+	{
+		_putchar(i);
+		_putchar('\n')
+	}
+	return (0);
+}
