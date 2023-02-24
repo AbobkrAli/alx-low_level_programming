bbcc4627e7cea1f2a2e4f36bf3928ce79e1a5aad
abobkr ali <abobkracc@gmail.com>
2023-02-24 20:30:24 +0200

    adding files

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
-#include <stdio>
+#include "main.h"
 
 /**
-*main - check if it's uppercase
-*Return: (0) if success
-*/
-
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
